6513ce93cf2307bcafe5b66211238f343d14440c
aruncs31s <aruncs31ss@gmail.com>
2022-02-20 23:28:24 +0530

    update

--- theme-manager/theme-manager.sh
+++ theme-manager/theme-manager.sh
@@ -14,14 +14,17 @@ cat <<- EOF
 
 	EOF
 echo "T-ui Expert Theme Manager"
+######
 tui=/storage/emulated/0/t-ui
 sd=/storage/emulated/0
 sdt=/storage/emulated/0/tui-themes
+######
 theme1=DRACULA_THEME_2022
 theme2=Grid_theme
 theme_3=WallX_theme
 theme_4=Tomboy.girl_theme
 theme_5=ff
+######
 usage(){
 printf "Usage \ntui-theme show <to --show the list of themes>\ntui-theme number_of_the_theme \nfor example tui-theme 1 <this will install DRACULA_THEME_2022"
 }
@@ -38,21 +41,33 @@ install_1(){
         cd $sdt && curl -LJO https://github.com/M4dGun/t-ui_themes/raw/main/Dracula_theme/DRACULA_THEME_2022.zip
 
     	else 
-        cd $sdt && wget https://github.com/M4dGun/t-ui_themes/raw/main/Dracula_theme/DRACULA_THEME_2022.zip 
+        cd $sdt && wget https://github.com/M4dGun/t-ui_themes/raw/main/Dracula_theme/DRACULA_THEME_2022.zip &>/dev/null ;
 	fi
+	printf "\nFinished Downloading\n" ;
+	sleep 2.0
+	printf "\nInstalling ." ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "." 
   	mkdir $tui
   	unzip -o  DRACULA_THEME_2022.zip -d $tui ; cd $tui/$theme1  && cp * $tui  ;
+	printf "\nfinished Installing\n"
+	sleep 1.0 
+	printf "\nNow type restart in t-ui \n"
 }
 install_2(){
-     mv /storage/emulated/0/t-ui /storage/emulated/0/tui.bak
+	if [[ $sdt ]]; then
+	echo "Updating " ; sleep 3.0
+	else
+	mkdir $sdt &>/dev/null
+	fi
+    mv $tui $sd/tui.bak &>/dev/null
+	printf "\nDownloading theme .. Waite it will take time\n" ;
     if [[(-f /bin/curl )]]; then 
-    
-        cd /sdcard && curl -LO https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip
+        
+        cd $sd && curl -LO https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip
 
     
     else 
     
-        cd /sdcard && wget https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip &> /dev/null ;
+        cd $sd && wget https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip &>/dev/null ;
     
   fi
     unzip -o  t-ui_grid.zip -d $tui ; cd $tui/tui  && cp * $tui  ;
