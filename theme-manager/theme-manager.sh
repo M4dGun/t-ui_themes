236f4bce17d1f0bd8499e71c8f51722c7dd478f7
Arun cs <87601622+aruncs31s@users.noreply.github.com>
2022-02-20 23:35:34 +0530

    Update theme-manager.sh

--- theme-manager/theme-manager.sh
+++ theme-manager/theme-manager.sh
@@ -59,18 +59,20 @@ install_2(){
 	mkdir $sdt &>/dev/null
 	fi
     mv $tui $sd/tui.bak &>/dev/null
+    
 	printf "\nDownloading theme .. Waite it will take time\n" ;
     if [[(-f /bin/curl )]]; then 
         
-        cd $sd && curl -LO https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip
-
+        cd $sd && curl -LO https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip?raw=true &>/dev/null
+	printf "\nFinished Downloading\n"
     
     else 
     
         cd $sd && wget https://github.com/M4dGun/t-ui_themes/blob/main/Grid_theme/t-ui_grid.zip &>/dev/null ;
     
   fi
-    unzip -o  t-ui_grid.zip -d $tui ; cd $tui/t-ui  && cp * $tui  ;
+  printf "\nInstalling ." ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "."  ; sleep 0.5 ;printf "." 
+  unzip -o  t-ui_grid.zip -d $tui ; cd $tui/t-ui  && cp * $tui  ;
 
 }
 show(){
